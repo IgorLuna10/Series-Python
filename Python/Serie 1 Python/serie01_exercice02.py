aefe77641a03eee5d06cb3182ff86b73dbd18416
Igor Luna <120457770+IgorLuna10@users.noreply.github.com>
2025-12-01 15:22:43 +0100

    Update print statement for price calculation output

--- Python/Serie 1 Python/serie01_exercice02.py
+++ Python/Serie 1 Python/serie01_exercice02.py
@@ -4,5 +4,5 @@ taux_tva = float(input("Entrez le taux de TVA (en pourcentage) : "))
 tva = prix_ht * (taux_tva / 100)
 prix_ttc = prix_ht + tva
 
-print(f"Pour un prix HT de {prix_ht}€ et un taux de TVA de {taux_tva}%, le prix TTC est de {prix_ttc}€.")
+print(f"Prix HT de {prix_ht}€ et un taux de TVA de {taux_tva}%, le prix TTC est de {prix_ttc}€.")
 
